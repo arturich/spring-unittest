a6a32a60717103d0f4bc2e453cc2f6aefe02f3f5
arturich <arturich@gmail.com>
2022-04-26 17:50:21 -0500

    Adding test cases using assertEquals and assertNotEquals

--- src/test/java/com/luv2code/test/ApplicationExampleTest.java
+++ src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -1,18 +1,75 @@
 package com.luv2code.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.luv2code.component.MvcTestingExampleApplication;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
 
 //It uses main Spring boot app
 @SpringBootTest(classes= MvcTestingExampleApplication.class)
 public class ApplicationExampleTest {
 	
-	@Test
-	void basicTest()
+	private int count = 0;
+	
+	@Value("${info.app.name}")
+	private String appInfo;
+	
+	@Value("${info.app.description}")
+	private String appDescription;
+	
+	@Value("${info.app.version}")
+	private String appVersion;
+	
+	@Value("${info.school.name}")
+	private String schoolName;
+	
+	@Autowired
+	CollegeStudent student;
+	
+	@Autowired
+	StudentGrades studentGrades;
+	
+	@BeforeEach
+	public void beforeEach()
 	{
+		count++;
+		System.out.println("Testing: " + appInfo + " which is "+ appDescription +
+				" version: " + appVersion + ", Execution count: " + count);
 		
+		student.setFirstname("Eric");
+		student.setLastname("Roby");
+		student.setEmailAddress("roby@citalin.com");
+		studentGrades.setMathGradeResults(new ArrayList<>(Arrays.asList(100.0,85.0,76.50,91.75)));
+		student.setStudentGrades(studentGrades);
+	}
+	
+	
+	@Test
+	@DisplayName("Add grade results for student grades")	
+	void addGradeResultsForStudentsGrades()
+	{
+		assertEquals(353.25,studentGrades.addGradeResultsForSingleClass(
+				student.getStudentGrades().getMathGradeResults()));
+	}
+	
+	@Test
+	@DisplayName("Add grade results for student grades not equal")	
+	void addGradeResultsForStudentsGradesAssertNotEquals()
+	{
+		assertNotEquals(0,studentGrades.addGradeResultsForSingleClass(
+				student.getStudentGrades().getMathGradeResults()));
 	}
 
 }
