6138a95eb2a5ce24df46ea56d4570b98cd1adba4
arturich <arturich@gmail.com>
2022-04-26 17:19:59 -0500

    Using different path on test cases than in main and adding configuration to work the tests

---
+++ src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -0,0 +1,18 @@
+package com.luv2code.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.luv2code.component.MvcTestingExampleApplication;
+
+//It uses main Spring boot app
+@SpringBootTest(classes= MvcTestingExampleApplication.class)
+public class ApplicationExampleTest {
+	
+	@Test
+	void basicTest()
+	{
+		
+	}
+
+}
